16b45d3c3b07635046f45638274b94815e8ea7a9
calebxyz <calebxyz@gmail.com>
2022-08-15 10:25:06 +0300

    Update Invoker.hpp

--- Invoker.hpp
+++ Invoker.hpp
@@ -9,11 +9,13 @@ struct Invoker{
     requires std::convertible_to<I, Invocable_t>
     Invoker(I invocable): 
     invocable_(std::move(invocable)) {}; 
+    
     template <typename... Args>
     requires std::invocable<Invocable&, Args...>
     decltype(auto) operator()(Args&&... args){
         return std::invoke(invocable_, std::forward<Args>(args)...);
     }
+    
     private: 
     Invocable invocable_; 
 };

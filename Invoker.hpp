cb38ab5d9945bce91a5a723b01da15366918b2e4
Muhammed Galib Uludag <m231@tnctr.com>
2022-08-16 02:05:23 +0300

    Added C++17 support

--- Invoker.hpp
+++ Invoker.hpp
@@ -1,25 +1,43 @@
+#ifndef INVOKER_HEADER
+#define INVOKER_HEADER
+
+#ifdef _MSC_VER
+#undef __cplusplus
+#define __cplusplus _MSVC_LANG
+#endif
+
+#if __cplusplus > 201402L
 #include <type_traits> 
-#include <concepts>
 #include <functional>
+#if __cplusplus > 201703L
+#include <concepts>
+#endif // __cplusplus > 201703L
 
 template <typename Invocable>
 struct Invoker{
-    using Invocable_T = std::conditional_t<std::is_function_v<Invocable>, 
-    std::add_pointer_t<Invocable>, Invocable>;
-    template <typename I> 
+    using Invocable_T = std::conditional_t<std::is_function_v<Invocable>, std::add_pointer_t<Invocable>, Invocable>;
+#if __cplusplus > 201703L
+    template <typename I>
     requires std::convertible_to<I, Invocable_T>
-    Invoker(I invocable): 
-    invocable_(std::move(invocable)) {} 
+#else // __cplusplus < 202002L
+    template <typename I, typename = typename std::enable_if<std::is_convertible<I, Invocable_T>::value>::type>
+#endif // __cplusplus > 201703L
+    Invoker(I invocable) : invocable_(std::move(invocable)) {}
     
     template <typename... Args>
+#if __cplusplus > 201703L
     requires std::invocable<Invocable&, Args...>
+#endif // __cplusplus > 201703L
     decltype(auto) operator()(Args&&... args){
         return std::invoke(invocable_, std::forward<Args>(args)...);
     }
     
-    private: 
-    Invocable_T invocable_; 
+private:
+    Invocable_T invocable_;
 };
 
 template <typename I>
 Invoker(I) -> Invoker<I>;
+#endif // __cplusplus > 201402L
+
+#endif // INVOKER_HEADER
